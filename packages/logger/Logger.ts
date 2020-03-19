b83908c27778f0a4e83e5992905ea3e18e9c69a7
Udhaya Kumar <Udhaya_Kumar@amat.com>
2020-03-19 14:41:38 +0530

    fix(logger): convert message param to string

--- packages/logger/Logger.ts
+++ packages/logger/Logger.ts
@@ -2,19 +2,19 @@ export class Logger {
     constructor() {}
 
     log(message) {
-        console.log(message);
+        console.log(message.toString());
     }
 
     info(message) {
-        console.info(message);
+        console.info(message.toString());
     }
 
     error(message) {
-        console.error(message);
+        console.error(message.toString());
     }
 
     warning(message) {
-        console.warn(message);
+        console.warn(message.toString());
     }
 
     clear() {
